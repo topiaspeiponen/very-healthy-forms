2c0c3d1f9c1c1c3b90347b20abaccbfc0a30ff50
topiaspeiponen <topias.peiponen@gmail.com>
2024-10-06 12:01:27 +0300

    perf: lazy load components to improve code splitting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense, lazy } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css'
-import Root from './routes/root.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import FillForm from './routes/fill-form/fill-form.tsx';
-import ViewForms from './routes/view-forms/view-forms.tsx';
 import createTheme from '@mui/material/styles/createTheme';
 import { ThemeProvider } from '@emotion/react';
-import ViewForm from './routes/view-form/view-form.tsx';
 import ErrorPage from './components/ErrorPage.tsx';
+import CircularProgress from '@mui/material/CircularProgress';
+
+// Lazy load different views to improve bundle size
+const Root = lazy(() => import('./routes/root.tsx'));
+const FillForm = lazy(() => import('./routes/fill-form/fill-form.tsx'));
+const ViewForms = lazy(() => import('./routes/view-forms/view-forms.tsx'));
+const ViewForm = lazy(() => import('./routes/view-form/view-form.tsx'));
+
 
 const theme = createTheme({
   palette: {
@@ -31,21 +35,38 @@ const theme = createTheme({
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: (
+      <Suspense fallback={<CircularProgress thickness={5} />}>
+        <Root />
+      </Suspense>
+    ),
     errorElement: <ErrorPage text="Jokin meni pieleen" />
   },
   {
     path: "/fill-form/:formSlug",
-    element: <FillForm />
+    element: (
+      <Suspense fallback={<CircularProgress thickness={5} />}>
+        <FillForm />
+      </Suspense>
+    )
   },
   {
     path: "/view-forms",
-    element: <ViewForms />,
+    element: (
+      <Suspense fallback={<CircularProgress thickness={5} />}>
+        <ViewForms />
+      </Suspense>
+    )
   },
 
   {
     path: "/view-forms/:formId",
-    element: <ViewForm />
+    element: (
+
+      <Suspense fallback={<CircularProgress thickness={5} />}>
+        <ViewForm />
+      </Suspense>
+    )
   }
 ]);
 
@@ -53,6 +74,7 @@ const client = new ApolloClient({
   uri: 'https://localhost:7500/graphql',
   cache: new InMemoryCache(),
 });
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
